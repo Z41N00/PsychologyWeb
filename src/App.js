3858d0d95a3c8f592a29e6713be659c87effd815
Z41N00 <zainebrahim1970@gmail.com>
2026-05-05 20:26:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
+);
+
 import { useState, useEffect } from "react";
 
 const C = {
